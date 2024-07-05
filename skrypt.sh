667dda7f46e44cce786deee48e0fcd00c2ef7b7d
daria <dariuszakasprzyk@gmail.com>
2024-07-05 12:26:35 +0200

    add --logs

--- skrypt.sh
+++ skrypt.sh
@@ -2,3 +2,11 @@
 if [ "$1" == "--date" ]; then
   date
 fi
+if [ "$1" == "--logs" ]; then
+  count=${2:-100}
+  for i in $(seq 1 $count); do
+    echo "log${i}.txt" > log${i}.txt
+    echo "$0" >> log${i}.txt
+    date >> log${i}.txt
+  done
+fi
